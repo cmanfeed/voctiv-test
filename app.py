f676795cf61a3b48b6806c6801d2cf21ef7aa17d
caiomanfredini <caiomanfredini.15@gmail.com>
2021-08-16 18:27:45 -0300

    final commit

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from requestform import RequestForm
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    return render_template('index.html')
+
+
+@app.route("/search",  methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        rq = RequestForm(request.form['address'])
+        distance = rq.call_func()
+
+        return render_template('index.html', result=distance)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ requestform.py
@@ -0,0 +1,44 @@
+import requests
+import haversine
+
+
+class RequestForm:
+    def __init__(self, stpoint, edpoint='МКАД'):
+        # setup the parameters to make a request
+        self.api_key = '9051fbbb-da35-4559-8a75-0cdf78717b74'
+        self.url_bse = 'https://geocode-maps.yandex.ru/1.x/?'
+        # start point
+        self.stpoint = stpoint
+        # endpoint -> default = 'МКАД' (Moscow Ring Road)
+        self.edpoint = edpoint
+
+    def get_response(self, point):
+        """
+        Make a request and convert to json
+        """
+        return requests.get(
+            self.url_bse,
+            params={
+                'apikey': self.api_key,
+                'geocode': point,
+                'format': 'json'
+            }
+        ).json()
+
+    def get_coord(self, response):
+        try:
+            distance = response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(
+            ' ')
+        except:
+            distance = [0, 0]
+        
+        return (float(distance[0]), float(distance[1]))
+
+    def get_distance(self, coordA, coordB):
+        return haversine.haversine((coordA), (coordB))
+    
+    def call_func(self):
+        coordA = self.get_coord(self.get_response(self.stpoint))
+        coordB = self.get_coord(self.get_response(self.edpoint))
+
+        return self.get_distance(coordA, coordB)
